6027050e795d0c91cbf75726b69fc3707c089618
azzakaa <azzaka24@gmail.com>
2026-01-07 12:10:19 +0700

    fix(update-kriteria) : perbaikan logika update

--- pages/kriteria/aksi_update.php
+++ pages/kriteria/aksi_update.php
@@ -1,14 +1,26 @@
 <?php
-include '../../lib/koneksi.php';
-$id_kriteria = $_GET['id_kriteria'];
-$kriteria = $_POST['kriteria'];
-$type = $_POST['type'];
-$bobot = $_POST['bobot'];
-$sql = "UPDATE tabel_kriteria SET kriteria='$kriteria',type='$type',bobot='$bobot' WHERE id_kriteria=$id_kriteria";
+// Pastikan path koneksi benar
+include "../../lib/koneksi.php";
 
-if ($koneksi->query($sql) === TRUE) {
-    echo "<script>alert('UPDATE berhasil');window.location = '../../index.php?module=list_kriteria';</script>";
+// Tangkap data dari POST (sekarang id_kriteria sudah ada isinya karena input hidden tadi)
+$id_kriteria = $_POST['id_kriteria'];
+$kriteria    = $_POST['kriteria'];
+$type        = $_POST['type'];
+$prioritas   = $_POST['prioritas'];
+
+// Query Update
+$sql = "UPDATE tabel_kriteria SET 
+            kriteria = '$kriteria',
+            type     = '$type',
+            prioritas= '$prioritas' 
+        WHERE id_kriteria = '$id_kriteria'";
+
+// Eksekusi
+if (mysqli_query($koneksi, $sql)) {
+    // Redirect ke halaman list agar perhitungan ROC otomatis berjalan ulang
+    header("location:../../index.php?module=list_kriteria");
 } else {
-    echo "Error: " . $sql . "<br>" . $koneksi->error;
+    echo "Gagal Update: " . mysqli_error($koneksi);
+    echo "<br><a href='../../index.php?module=list_kriteria'>Kembali</a>";
 }
 ?>
--- pages/kriteria/update_kriteria.php
+++ pages/kriteria/update_kriteria.php
@@ -6,47 +6,44 @@ $row = mysqli_fetch_assoc($result);
 ?>
 
             <div class="form-panel">
-              <h4 class="mb"><i class="fa fa-angle-right"></i> Tambah Kriteria</h4>
-              <form class="form-horizontal style-form" method="POST" action="pages/kriteria/aksi_update.php?id_kriteria=<?=$row['id_kriteria']?>">
-                <div class="form-group">
-                  <label class="col-sm-2 col-sm-2 control-label">Nama Kriteria</label>
-                  <div class="col-sm-10">
-                    <input type="text" class="form-control round-form" name="kriteria" value="<?=$row['kriteria']?>" required>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 col-sm-2 control-label">Type Kriteria</label>
-                  <div class="col-sm-10">
-                    <div class="form-check-inline">
-                      <label class="form-check-label" for="radio1">
-                        <?php 
-                          if ($row['type']==='benefit') {
-                        ?>
-                            <input type="radio" class="form-check-input" id="radio1" name="type" value="benefit" checked> Benefit
-                            <input type="radio" class="form-check-input" id="radio1" name="type" value="cost"> Cost
-                         <?php
-                          }else{
-                          ?>
-                            <input type="radio" class="form-check-input" id="radio1" name="type" value="benefit"> Benefit
-                            <input type="radio" class="form-check-input" id="radio1" name="type" value="cost" checked> Cost
-                        <?php
-                          }
-                         ?>
-                      </label>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 col-sm-2 control-label">Bobot Kriteria</label>
-                  <div class="col-sm-10">
-                    <input type="number" name="bobot" class="form-control round-form" min="0" step="0.1" value="<?=$row['bobot']?>" required>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-sm-12" style="text-align: center;">
-                    <button type="submit" class="btn btn-theme03">Masukan</button>
-                    <button type="reset" class="btn btn-theme04">Reset</button>
-                  </div>
-                </div>
-              </form>
-            </div>
+  <h4 class="mb"><i class="fa fa-angle-right"></i> Tambah/Ubah Kriteria</h4>
+  
+  <form class="form-horizontal style-form" method="POST" action="pages/kriteria/aksi_update.php">
+    
+    <input type="hidden" name="id_kriteria" value="<?=$row['id_kriteria']?>">
+    <div class="form-group">
+      <label class="col-sm-2 col-sm-2 control-label">Nama Kriteria</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control round-form" name="kriteria" value="<?=$row['kriteria']?>" required>
+      </div>
+    </div>
+    
+    <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Type Kriteria</label>
+        <div class="col-sm-10">
+        <div class="form-check-inline">
+                  <label class="form-check-label">
+                    <input type="radio" name="type" value="benefit" <?php if($row['type']=='benefit') echo 'checked'; ?>> Benefit
+                    <input type="radio" name="type" value="cost" <?php if($row['type']=='cost') echo 'checked'; ?>> Cost
+                  </label>
+             </div>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label class="col-sm-2 col-sm-2 control-label">Prioritas (Ranking)</label>
+        <div class="col-sm-10">
+            <input type="number" name="prioritas" class="form-control" value="<?php echo $row['prioritas']; ?>" required>
+            <span class="help-block">Masukkan angka 1 untuk prioritas tertinggi, dst.</span>
+        </div>
+    </div>
+
+    <div class="form-group">
+      <div class="col-sm-12" style="text-align: center;">
+        <button type="submit" class="btn btn-theme03">Simpan Perubahan</button>
+        <a href="index.php?module=list_kriteria" class="btn btn-theme04">Batal</a>
+      </div>
+    </div>
+
+  </form>
+</div>
